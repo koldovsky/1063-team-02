4dc9aa19de608f526fd75542cdf14d2980a904d5
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-09-24 11:41:56 +0300

    add burger button js

--- js/header.js
+++ js/header.js
@@ -1,20 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("burger").addEventListener("click", function () {
-        document.querySelector(".header__navbar").classList.toggle("open");
-    });
+document.getElementById("burger-btn").addEventListener("click", function () {
+    document.querySelector(".header__navbar").classList.toggle("open");
 });
-window.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-        document.querySelector("header__navbar").classList.remove("open");
-    }
-});
-document.getElementById("navbar__menu").addEventListener("click", (event) => {
-    event._isClickWithInMenu = true;
-});
-document.getElementById("burger").addEventListener("click", (event) => {
-    event._isClickWithInMenu = true;
-});
-document.body.addEventListener("click", (event) => {
-    if (event._isClickWithInMenu) return;
-    document.querySelector(".header__navbar").classList.remove("open");
-});
+
+const clockContainer = document.querySelector('.header__clock');
+function updateClock() {
+    clockContainer.innerText = new Date().toLocaleDateString('uk');
+}
+setInterval(updateClock, 1000);
+updateClock();
+
+const headerText = document.querySelector('.header__title');
+const greetings = [
+    'Hi Team!',
+    'Hi Friends!',
+    'Hello everybody!',
+];
+function getRandomEl(arr) {
+    const randomIdx = Math.floor(Math.random() * arr.length);
+    return arr[randomIdx];
+}
+headerText.innerText = getRandomEl(greetings);
