641b644e0f3f0ad7b761896e7116c68ef7993fd4
Taras Bohoslavets <bogoslavetstaras@gmail.com>
2023-09-27 11:14:14 +0300

    added anchors to make links work

--- js/header.js
+++ js/header.js
@@ -1,14 +1,51 @@
-document.getElementById("burger-btn").addEventListener("click", function () {
-    const mainElement = document.querySelector("main");
-    mainElement.toggleAttribute("hidden");
-    const bodyElement = document.querySelector("body");
-    if (bodyElement.style.overflow === "hidden") {
-        bodyElement.style.overflow = "";
+// document.getElementById("burger-btn").addEventListener("click", function () {
+//     document.querySelector(".header__navbar").classList.toggle("open");
+// });
+
+const burgerBtn = document.getElementById("burger-btn");
+const mainElement = document.querySelector("main");
+const bodyElement = document.querySelector("body");
+const navbar = document.querySelector(".header__navbar");
+const contentToHide = document.querySelectorAll(".content-to-hide");
+
+burgerBtn.addEventListener("click", function () {
+    mainElement.classList.toggle("hidden");
+    navbar.classList.toggle("open");
+
+    // Приховати/показати інший контент
+    contentToHide.forEach(function(element) {
+        element.classList.toggle("hidden");
+    });
+
+    if (navbar.classList.contains("open")) {
+        bodyElement.style.overflowY = "hidden"; // Вимкнути вертикальну прокрутку
     } else {
-        bodyElement.style.overflow = "hidden";
+        bodyElement.style.overflowY = "auto"; // Увімкнути вертикальну прокрутку
     }
-    document.querySelector(".header__navbar").classList.toggle("open");
 });
+
+const links = document.querySelectorAll(".header__navbar a");
+
+links.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+        mainElement.classList.add("hidden");
+        navbar.classList.remove("open");
+
+        // Приховати інший контент
+        contentToHide.forEach(function(element) {
+            element.classList.add("hidden");
+        });
+
+        bodyElement.style.overflowY = "auto"; // Увімкнути вертикальну прокрутку
+
+        const targetSectionId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetSectionId);
+
+        targetSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+
 
 
 const clockContainer = document.querySelector('.header__clock');
