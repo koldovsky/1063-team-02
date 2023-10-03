78bed17124679d07fe5959b2c1ad9e66a54facca
LevPredko <136936755+LevPredko@users.noreply.github.com>
2023-10-03 09:56:28 +0000

    added JS for main-collection

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ document.addEventListener('partialsLoaded', () => {
     import('./header.js');
     import('./brands.js');
     import('./new-arrivals.js');
+    import('./main-collection.js');
 });
